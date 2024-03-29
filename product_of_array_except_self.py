b59d6a52c66c6f6c7c51b84f064fa51aeb7a060e
AbdallahZein12 <abdallahabdelmoneim7@gmail.com>
2024-03-29 14:40:38 -0400

    Added additional solution to product of array except

--- product_of_array_except_self.py
+++ product_of_array_except_self.py
@@ -18,3 +18,27 @@ class Solution(object):
             postfix *= nums[i]
         return res
         
+###
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        length = len(nums)
+        answer = [1] * length
+        left = [1] * length
+        right = [1] * length
+
+        left[0] = 1
+        for i in range(1,length):
+            left[i] = left[i-1] * nums[i-1]
+
+        right[-1] = 1
+        for i in range(length-2,-1,-1):
+            right[i] = right[i+1] * nums[i+1]
+
+        answer = [x * y for x,y in zip(left,right)]
+
+        return answer
