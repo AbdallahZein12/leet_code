cda28a2ca32403ff2eb49f9577843d42ba6bf985
AbdallahZein12 <abdallahabdelmoneim7@gmail.com>
2024-04-14 16:50:04 -0400

    Added 2 solutions to carfleet problem

---
+++ carfleet.py
@@ -0,0 +1,29 @@
+#### Stack Solution 
+
+class Solution:
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        pair = [[p,s] for p,s in zip(position,speed)]
+
+        stack = []
+        for p,s in sorted(pair)[::-1]: # Reverse Sorted Order 
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+
+        return len(stack)
+    
+### Faster solution
+
+class Solution:
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        
+        pairs = [(position[i], speed[i]) for i in range(len(position))]
+        fleets = curtime = 0  # a car's position is always < than target at the start, so it's fine to start curtime at 0 (no fleet will be at target at time 0)
+        
+        for dist, speed in sorted(pairs, reverse=True):
+            destination_time = (target - dist)/speed
+            if curtime < destination_time:
+                fleets += 1
+                curtime = destination_time
+
+        return fleets
