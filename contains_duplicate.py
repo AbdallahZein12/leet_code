085f6270a51562de1b108ebc8a13aec761537449
AbdallahZein12 <abdallahabdelmoneim7@gmail.com>
2024-09-23 16:11:08 -0400

    Added 2 extra solutions

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -33,3 +33,31 @@ print(solution.containsDuplicate(nums=[1,2,3,4,4]))
     
 ####
 
+
+## Time O(n^2) Space O(1)
+class containsDuplicate:
+    def solution(self, nums: list[int]) -> bool:
+        for i in range(len(nums)):
+            if nums[i] in nums[:i] + nums[i+1:]:
+                return True
+        return False 
+    
+cls = containsDuplicate()
+print(cls.solution(nums=[1,1,2,3,4,5]))
+
+## Time O(n) Space O(n)
+class containsDuplicate:
+    def solution(self, nums: list[int]) -> bool:
+        hashset = set()
+        for i in nums:
+            if i in hashset:
+                return True
+            hashset.add(i)
+        return False 
+    
+    
+    
+    
+             
+                
+            
