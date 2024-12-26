f2cf667e0cd04a3c53c8b60e4b1770d59e5ff4e1
AbdallahZein12 <abdallahabdelmoneim7@gmail.com>
2024-12-26 05:17:58 -0500

    Added reverse_between ll solution

--- valid_anagram.py
+++ valid_anagram.py
@@ -30,3 +30,31 @@ from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return Counter(s) == Counter(t)
+    
+    
+    
+class validAnagram:
+    def solution(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False 
+        
+        dict_s = dict()
+        dict_t = dict()
+        
+        for i in range(len(s)):
+            dict_s[s[i]] = dict_s.get(s[i],0) + 1 
+            dict_t[t[i]] = dict_t.get(t[i],0) + 1 
+        
+        print(dict_s,"\n\n\n\n",dict_t)
+        
+        print(dict_s == dict_t)
+                 
+        for i in dict_t:
+            if dict_t[i] != dict_s.get(i,0):
+                return False 
+        
+        return True  
+    
+cls = validAnagram()
+print(cls.solution(s='racecar', t='carrace'))
